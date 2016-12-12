15aa7cb3a7e911d1ed25a24c69c9d681b6ce85b7
Nick McVey <nsmcvey@gmail.com>
2016-12-12 12:59:15 -0700

    sort of working
    Well…its now readying the script

--- week 5/starter_code/js/main.js
+++ week 5/starter_code/js/main.js
@@ -9,13 +9,9 @@
     lastly, alert with no city give random picture
     
     */
-
 console.log('youre getting this far');
 
-var cityInput = $('#city-type').val();
-
-
-function changeBackground(event) {
+function changeBackground() {
     event.preventDefault();
     if (cityInput == "NYC" || cityInput == "New York City" || cityInput == "New York") {
         //change background to NYC
@@ -25,30 +21,29 @@ function changeBackground(event) {
     else if (cityInput == "San Francisco" || cityInput == "Bay Area" || cityInput == "SF") {
         //change background to SF
         $('body').css("background-image", "url (../images/sf.jpg) no-repeat center center fixed");
-         console.log("changed to SF");
+        console.log("changed to SF");
     }
     else if (cityInput == "LA" || cityInput == "Los Angeles" || cityInput == "LAX") {
         //change background to LA
         $('body').css("background-image", "url (../images/la.jpg) no-repeat center center fixed");
-         console.log("changed to LA");
+        console.log("changed to LA");
     }
     else if (cityInput == "Austin" || cityInput == "ATX") {
         //change background to austin
         $('body').css("background-image", "url (../images/austin.jpg) no-repeat center center fixed");
-         console.log("changed to Austin");
+        console.log("changed to Austin");
     }
     else if (cityInput == "Sydney" || cityInput == "SYD") {
         //change background to Sydney
         $('body').css("background-image", "url (../images/sydney.jpg) no-repeat center center fixed");
-         console.log("changed to Sydney");
-    }
-    else {
-        $('body').css("background-image", "url (../images/citipix_skyline.jpg) no-repeat center center fixed");
-         console.log("changed to original");
+        console.log("changed to Sydney");
     }
 };
 
-$('#submit-btn').click(changeBackground);
-
-console.log(cityInput);
-    
+$("#submit-btn").on("click", function (event) {
+    event.preventDefault();
+    changeBackground();
+    console.log(cityInput);
+});
+var cityInput = $('#city-type').val();
+//$('#submit-btn').click(changeBackground);
