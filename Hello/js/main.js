ee86d089d3754c9525b04a45fbd78e2d223cd119
Nick McVey <nsmcvey@gmail.com>
2017-01-04 18:29:29 -0700

    small updates
    version of sequential background

--- Hello/js/main.js
+++ Hello/js/main.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
 
 console.log("Hello")
 
-
+function nextSlide() {
+    var q = function(sel) { return document.querySelector(sel); }   
+    q(".slides").appendChild(q(".slides img:first-child"));
+}
+setInterval(nextSlide, 3000)
 
 
 })
