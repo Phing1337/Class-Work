b0f7aa358c7de0b7b3b2fe199a759c8c2a081e4c
Nick McVey <nsmcvey@gmail.com>
2017-01-23 21:28:08 -0700

    working
    working - still need to work on fullscreen

--- Hello/js/main.js
+++ Hello/js/main.js
@@ -18,4 +18,63 @@ console.log("Hello");
 // then fade in blur and dark background
 // then fade in page info
     
+    $(".nav1").click(function(){
+    $(".content").fadeToggle();
+    $('.fill').fadeToggle();
+});
+    function fullscreen()
+      {
+        
+        var fullscrn = document.getElementsByClassName("fullscreen")[0];
+        req= fullscrn.requestFullScreen || fullscrn.webkitRequestFullScreen || fullscrn.mozRequestFullScreen;
+        req.call(fullscrn);
+          
+
+
+      }
+    $('.full').click(fullscreen);
+    
+    
+    $('.xde').click(function(){
+            $('.inactive').css({"color":"white"});
+            $('.xde').css({"color":"orange"});
+              $('.bio').hide();
+              $('#xde').fadeIn();
+        });
+    $('.tax').click(function(){
+        $('.inactive').css({"color":"white"});
+            $('.tax').css({"color":"orange"});
+              $('.bio').hide();
+              $('#tax').fadeIn();
+        });
+   $('.workday').click(function(){
+       $('.inactive').css({"color":"white"});
+            $('.workday').css({"color":"orange"});
+              $('.bio').hide();
+              $('#workday').fadeIn();
+        });
+    $('.service').click(function(){
+        $('.inactive').css({"color":"white"});
+            $('.service').css({"color":"orange"});
+              $('.bio').hide();
+              $('#service').fadeIn();
+        });
+    $('.oracle').click(function(){
+        $('.inactive').css({"color":"white"});
+            $('.oracle').css({"color":"orange"});
+              $('.bio').hide();
+              $('#oracle').fadeIn();
+        });
+    $('.tbm').click(function(){
+        $('.inactive').css({"color":"white"});
+            $('.tbm').css({"color":"orange"});
+              $('.bio').hide();
+              $('#tbm').fadeIn();
+        });
+    $('.lighthouse').click(function(){
+        $('.inactive').css({"color":"white"});
+            $('.lighthouse').css({"color":"orange"});
+              $('.bio').hide();
+              $('#lighthouse').fadeIn();
+        });
 });
