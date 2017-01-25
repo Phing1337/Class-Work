9dab8ca0526ef38ba28ec451d841354aa3493550
Nick McVey <nsmcvey@gmail.com>
2017-01-24 22:51:53 -0700

    complete
    completed Project

--- Hello/js/main.js
+++ Hello/js/main.js
@@ -19,62 +19,89 @@ console.log("Hello");
 // then fade in page info
     
     $(".nav1").click(function(){
+        toggleBlur();
     $(".content").fadeToggle();
     $('.fill').fadeToggle();
+    
 });
+    
+
+ function toggleBlur () {
+     if ($(".fill:hidden").length){
+           $('.swiper-slide').css({"filter":"blur(5px)"});
+       }
+        else
+             $('.swiper-slide').css({"filter":"blur(0px)"});
+  };
+    
+    
     function fullscreen()
       {
         
-        var fullscrn = document.getElementsByClassName("fullscreen")[0];
+        var fullscrn = document.getElementsByClassName("breaker")[0];
         req= fullscrn.requestFullScreen || fullscrn.webkitRequestFullScreen || fullscrn.mozRequestFullScreen;
         req.call(fullscrn);
           
 
 
       }
-    $('.full').click(fullscreen);
+    //$('.breaker').click(fullscreen);
     
     
     $('.xde').click(function(){
             $('.inactive').css({"color":"white"});
             $('.xde').css({"color":"orange"});
+            $('.fp').hide();
               $('.bio').hide();
               $('#xde').fadeIn();
+        $('#fp-xde').fadeIn();
         });
     $('.tax').click(function(){
         $('.inactive').css({"color":"white"});
             $('.tax').css({"color":"orange"});
               $('.bio').hide();
+                $('.fp').hide();
               $('#tax').fadeIn();
+                $('#fp-tax').fadeIn();
         });
    $('.workday').click(function(){
        $('.inactive').css({"color":"white"});
             $('.workday').css({"color":"orange"});
+       $('.fp').hide();
               $('.bio').hide();
               $('#workday').fadeIn();
+       $('#fp-workday').fadeIn();
         });
     $('.service').click(function(){
         $('.inactive').css({"color":"white"});
             $('.service').css({"color":"orange"});
+        $('.fp').hide();
               $('.bio').hide();
               $('#service').fadeIn();
+        $('#fp-service').fadeIn();
         });
     $('.oracle').click(function(){
         $('.inactive').css({"color":"white"});
             $('.oracle').css({"color":"orange"});
+        $('.fp').hide();
               $('.bio').hide();
               $('#oracle').fadeIn();
+        $('#fp-oracle').fadeIn();
         });
     $('.tbm').click(function(){
         $('.inactive').css({"color":"white"});
             $('.tbm').css({"color":"orange"});
+        $('.fp').hide();
               $('.bio').hide();
               $('#tbm').fadeIn();
+        $('#fp-tbm').fadeIn();
         });
     $('.lighthouse').click(function(){
         $('.inactive').css({"color":"white"});
             $('.lighthouse').css({"color":"orange"});
+        $('.fp').hide();
               $('.bio').hide();
               $('#lighthouse').fadeIn();
+        $('#fp-lighthouse').fadeIn();
         });
 });
